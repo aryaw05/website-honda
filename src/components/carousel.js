195c5acaf3b8a374fcec969976d21e43b5ad8900
arya05 <aryawahyuaw77@gmail.com>
2024-01-12 10:54:22 +0700

    last updates

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -37,12 +37,12 @@ function Corousel() {
               className="d-block w-100"
               alt="..."
             />
-            <div className="carousel-caption d-none d-md-block">
+            {/* <div className="carousel-caption d-none d-md-block">
               <h5>First slide label</h5>
               <p>
                 Some representative placeholder content for the first slide.
               </p>
-            </div>
+            </div> */}
           </div>
           <div className="carousel-item" data-bs-interval="2000">
             <img
@@ -50,12 +50,12 @@ function Corousel() {
               className="d-block w-100"
               alt="..."
             />
-            <div className="carousel-caption d-none d-md-block">
+            {/* <div className="carousel-caption d-none d-md-block">
               <h5>Second slide label</h5>
               <p>
                 Some representative placeholder content for the second slide.
               </p>
-            </div>
+            </div> */}
           </div>
           <div className="carousel-item">
             <img
@@ -63,12 +63,12 @@ function Corousel() {
               className="d-block w-100"
               alt="..."
             />
-            <div className="carousel-caption d-none d-md-block">
+            {/* <div className="carousel-caption d-none d-md-block">
               <h5>Third slide label</h5>
               <p>
                 Some representative placeholder content for the third slide.
               </p>
-            </div>
+            </div> */}
           </div>
         </div>
         <button
